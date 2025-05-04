c112a9b1de78a37b85c34eb6558910b44135bd1b
Kleijn95 <kleijn95@gmail.com>
2025-05-04 17:04:06 +0200

    added authentication folder

--- src/main/java/it/epicode/__05_2025_Progetto/eventi/EventoRequest.java
+++ src/main/java/it/epicode/__05_2025_Progetto/eventi/EventoRequest.java
@@ -1,5 +1,7 @@
 package it.epicode.__05_2025_Progetto.eventi;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -12,12 +14,20 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EventoRequest {
+
+    @NotBlank(message = "Il titolo non può essere vuoto")
     private String titolo;
+
+    @NotBlank(message = "La descrizione non può essere vuota")
     private String descrizione;
-    @NotBlank(message = "Il luogo non può essere non essere definito")
+
+    @NotBlank(message = "Il luogo non può essere vuoto")
     private String luogo;
-    @NotNull(message = "La data non può essere null")
+
+    @NotNull(message = "La data non può essere null")
+    @Future(message = "La data deve essere nel futuro")
     private LocalDate data;
-    @NotNull(message = "Il numero di posti disponibili non può essere null")
+
+    @Min(value = 1, message = "Deve esserci almeno un posto disponibile")
     private int numeroPostiDisponibili;
 }
