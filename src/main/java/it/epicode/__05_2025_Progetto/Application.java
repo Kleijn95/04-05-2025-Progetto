98f6b05708ad13465f57be159fb5aacd499bb482
Kleijn95 <kleijn95@gmail.com>
2025-05-04 16:28:57 +0200

    Initial commit

---
+++ src/main/java/it/epicode/__05_2025_Progetto/Application.java
@@ -0,0 +1,13 @@
+package it.epicode.__05_2025_Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
